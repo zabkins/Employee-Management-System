14a62c5fd88df123ee3ece523863bb358a45d31a
Michal Zarczynski <mzarczynski29@gmail.com>
2022-09-15 14:10:10 +0200

    Makes Employee an entity

--- data/src/main/java/pl/zarczynski/ems/domain/Employee.java
+++ data/src/main/java/pl/zarczynski/ems/domain/Employee.java
@@ -5,19 +5,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Entity
 @NoArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
+@Table(name = "EMPLOYEE")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String login;
     private String password;
     private String name;
     private String lastName;
-    private LocalDateTime hiredSince;
+    private LocalDate hiredSince;
+    @Enumerated(EnumType.STRING)
     private Position position;
     private int salary;
 }
