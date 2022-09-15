111804c91ce57641ccbb47bded47d30c0b2f40a7
Michal Zarczynski <mzarczynski29@gmail.com>
2022-09-15 14:14:38 +0200

    Adds basic WebSecurityConfig curently permitting all requests

---
+++ app/src/main/java/pl/zarczynski/ems/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package pl.zarczynski.ems.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .formLogin().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring().antMatchers("/**");
+    }
+
+}
